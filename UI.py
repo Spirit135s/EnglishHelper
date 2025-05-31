dc3122d7c2fe1f22f34ffb2a2846986d1a35b5b1
RayWu <wu2406@outlook.com>
2025-05-31 19:16:40 +0800

    8

--- UI.py
+++ UI.py
@@ -8,14 +8,13 @@ client = OpenAI(api_key="sk-c4d67b703ba447ed9bd593d71562103c", base_url="https:/
 
 
 topic="game"
-
-def chatbot(query, history):
-    messages = [{"role":"system", "content": "你叫做Alex,是一位英文学习助手，用CET4级英语和用户进行关于"+topic+"主题对话，每次回复不超过3句话，不多于40个单词,同时回答要求用中文总结用户的语法错误 \n 你在给出回复要求如下：1.不要使用Markdown格式 2.用包含Reply对象，Error数组和键值对布尔End的json文件返回你的回答。 3.在Reply对象，单独放置一个字符串，输出你给出的回答。 4.在Errors列表，将问题分类为“语法问题” “语句不通” “用词不当”，“习惯文化不符”,“逻辑混乱”部分。每个错误输出对象包含“问题分类”，“问题说明”，“修改建议”键值对,分别用problem, explain, suggestion表示。 5.如果你认为对话完全可以结束或者你就收到单独的一条“<END>”提示词,此时将返回的End项设为True,否则设置为False。"}]
-    for q, a in history:
-        messages.append({"role": "user", "content": q})
-        messages.append({"role": "assistant", "content": a})
-    messages = [{"role":"system", "content": """
-    你叫做Alex,是一位英文学习助手，用CET4级英语和用户进行关于"+topic+"主题对话，每次回复不超过3句话，不多于40个单词,同时回答要求用中文总结用户的语法错误 \n 你在给出回复要求如下：1.不要使用Markdown格式 2.用包含Reply对象，Error数组和键值对布尔End的json文件返回你的回答。 3.在Reply对象，单独放置一个字符串，输出你给出的回答。 4.在Errors列表，将问题分类为“语法问题” “语句不通” “用词不当”，“习惯文化不符”,“逻辑混乱”部分。每个错误输出对象包含“问题分类”，“问题说明”，“修改建议”键值对,分别用problem, explain, suggestion表示。 5.如果你认为对话完全可以结束或者你就收到单独的一条“<END>”提示词,此时将返回的End项设为True,否则设置为False。
+chat_prompt = """
+    你叫做Alex,是一位英文学习助手，用CET4级英语和用户进行对话，每次回复不超过4句话，不多于60个单词。回答分为两部分，一部分进行对话，一部分指出用户的错误。
+    你在给出回复要求如下：
+    1.用包含Reply键值对，Error数组和布尔键值对End的json文件返回你的回答。 
+    2.Reply键直接对应一条字符串，输出你给出的回答，不在其中提及用户的英文使用错误。
+    3.在Errors列表，将问题分类为“语法问题” “语句不通” “用词不当”，“习惯文化不符”,“逻辑混乱”部分。每个错误输出对象包含“问题分类”，“问题说明”，“修改建议”键值对,分别用problem, explain, suggestion表示。 
+    4.如果你认为对话完全可以结束或者你收到单独的一条“<END>”提示词,此时将返回的End项设为True,否则设置为False。
     
     EXAMPLE JSON OUTPUT:
     {
@@ -29,9 +28,67 @@ def chatbot(query, history):
         ],
         "End": false
     }
-    """}]
-    messages.append({"role": "user", "content": query})
+"""
 
+conclusion_prompt = """
+    你是一位英文老师，现在你有一段基于CET4级英语和包含用户和你进行对话的内容。基于以上的对话内容，对对话过程进行评价，总字数在100字左右，同时给出若干个你认为适合讨论话题的单词。
+    你的回答要求如下：
+    1.用包含Assess键值对和Vocabulary数组的json文件返回你的回答。 
+    2.Assess键对应一条字符串，以第二人称称呼用户，用中文输出你给出的评价。 
+    3.在Vocabulary数组，基于整个对话的长度，输出4-20个所有你推荐的值得用户学习掌握的单词，其中不包含单词缩写。每个单词对象要求包含“eng”，“chn”两个键值对，分别对应原英文单词和对应的中文释义
+
+    EXAMPLE JSON OUTPUT:
+    {
+        "Assess": "你在对话中使用了礼貌的语言，这利于学习交流。作为英语学习者，掌握礼貌用语和表达方式是非常重要的。建议你在未来的交流中保持礼貌，这样不仅能更好地学习英语，也能与他人建立良好的沟通关系。",
+        "Vocabulary": [
+            {
+            "eng": "Goodbye",
+            "chn": "再见"
+            },
+            {
+            "eng": "Farewell",
+            "chn": "告别"
+            },
+            {
+            "eng": "Polite",
+            "chn": "有礼貌的"
+            }
+        ]
+    }
+"""
+
+def combine_messages(prompt, query, history):
+    messages = []
+    messages.append({"role":"system", "content": chat_prompt})
+    for q, a in history:
+        messages.append({"role": "user", "content": q})
+        messages.append({"role": "assistant", "content": a})
+    messages.append({"role": "user", "content": query})
+    return messages
+
+def concludebot(history):
+    messages = []
+    messages.append({"role":"system", "content": conclusion_prompt})
+    for q, a in history:
+        messages.append({"role": "user", "content": q})
+        messages.append({"role": "assistant", "content": a})
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=messages,
+        response_format={
+            'type': 'json_object'
+        }
+    )
+    response_json=json.loads(response.choices[0].message.content)  # Ensure the response is in JSON format
+    assess = response_json["Assess"]  # Extract the assessment from the response
+    vocabulary = response_json["Vocabulary"]  # Extract the vocabulary list from the response
+    for word in vocabulary:
+        assess+= '\n'
+        assess+= f"单词: {word['eng']}, 释义: {word['chn']}"
+    return assess
+
+def chatbot(query, history):
+    messages = combine_messages(chat_prompt, query, history)
     response = client.chat.completions.create(
         model="deepseek-chat",
         messages=messages,
@@ -46,6 +103,10 @@ def chatbot(query, history):
     for err in error:
         print(f"问题分类: {err['problem']}, 问题说明: {err['explain']}, 修改建议: {err['suggestion']}")
     end = response_json["End"]  # Extract the end flag from the response
+
+    if end:
+        answer += "\n------end conversation------\n"
+        answer += concludebot(history)
     return answer  # Only return the answer, not the history
 
 gr.ChatInterface(chatbot, title="和Alex练习对话", description="请随意想一个话题，使用英文开始对话吧").launch()
