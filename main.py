7c98d217a8bac613e11f0497c89ef8eee344a029
Justin Sun <sunminghui0206@buaa.edu.cn>
2025-05-31 16:31:53 +0800

    Update 1

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from openai import OpenAI
 
 client = OpenAI(
-base_url='http://localhost:11434/v1',
+base_url='http://127.0.0.1:11434/v1',
 api_key='ollama', # 必填项，但在Ollama中会被忽略
 )
 
-def get_response(prompt,topic="basketball"):
+def get_response(prompt,topic="any"):
     response = client.chat.completions.create(
-        model="qwen2.5:7b",
+        model="qwen3:14b",
         messages=[
-            {"role": "system", "content": "你叫Alex，15岁美国学生。用初中级英语和用户进行"+topic+"主题对话，每次回复不超过2句话。对话结束后：1. 用中文总结用户的语法错误 2. 在自己的回答中向用户推荐3个相关新词汇"},
+            {"role": "system", "content": "你叫Alex，15岁美国学生。用CET4级英语和用户进行"+topic+"主题对话，每次回复不超过2句话。对话结束后：1. 用中文总结用户的语法错误 2. 在自己的回答中向用户推荐3个相关新词汇。请用Reply,Error,Vocabulary区分三个板块，用json格式返回你的回答。"},
             {"role": "user", "content": prompt},
         ]
     )
     return response.choices[0].message.content
 
-response=get_response("Who is LeBron James")  # Example usage
+response=get_response("Who is LeBron James","sports")  # Example usage
 print(response)
