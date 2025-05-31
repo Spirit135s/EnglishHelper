84b82bc0b2ae784401f9699ddc7ce6a2dfb2d80e
RayWu <wu2406@outlook.com>
2025-05-31 17:08:15 +0800

    4

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from openai import OpenAI
 
-'''client = OpenAI(
-base_url='http://localhost:10098/v1',
-api_key='ollama', # 必填项，但在Ollama中会被忽略
-)'''
+#client = OpenAI(base_url='http://localhost:10098/v1',api_key='ollama', # 必填项，但在Ollama中会被忽略)
+
 client = OpenAI(api_key="sk-c4d67b703ba447ed9bd593d71562103c", base_url="https://api.deepseek.com")
 
+
+def get_response(input_messages):
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=input_messages
+    )
+    return response
+
+def chat_training(topic="any"):
+    input_content = input("学生:")
+    messages=[{"role": "system", "content": "你叫Alex，15岁美国学生。用CET4级英语和用户进行"+topic+"主题对话，每次回复不超过2句话。对话结束后：1. 用中文总结用户的语法错误 2. 在自己的回答中向用户推荐3个相关新词汇。请用Reply,Error,Vocabulary区分三个板块，用json格式返回你的回答。"}]
+    while input_content!='exit':
+        messages.append({"role": "user", "content": input_content})
+        response = get_response(messages)
+        messages.append(response.choices[0].message)
+        print("AI:"+response.choices[0].message.content)
+        input_content = input("学生:")
+        #print("-------type",type(response))
+'''
 def get_response(prompt,topic="any"):
     response = client.chat.completions.create(
         model="deepseek-chat",
@@ -15,6 +32,7 @@ def get_response(prompt,topic="any"):
         ]
     )
     return response.choices[0].message.content
-
 response=get_response("Who is LeBron James","sports")  # Example usage
 print(response)
+'''
+chat_training(topic="game")
