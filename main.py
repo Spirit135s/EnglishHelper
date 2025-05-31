64d4c13d8dbb4a70bc88ee2337638f8c787394fb
RayWu <wu2406@outlook.com>
2025-05-31 17:19:36 +0800

    5

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def get_response(input_messages):
 
 def chat_training(topic="any"):
     input_content = input("学生:")
-    messages=[{"role": "system", "content": "你叫Alex，15岁美国学生。用CET4级英语和用户进行"+topic+"主题对话，每次回复不超过2句话。对话结束后：1. 用中文总结用户的语法错误 2. 在自己的回答中向用户推荐3个相关新词汇。请用Reply,Error,Vocabulary区分三个板块，用json格式返回你的回答。"}]
+    messages=[{"role": "system", "content": "你叫做Alex,是一位英文学习助手，用CET4级英语和用户进行关于"+topic+"主题对话，每次回复不超过3句话，不多于40个单词。一直保持对话进行知道你认为对话完全可以结束或者你就收到单独的一条“<END>”提示词。\n 你总共需要完成三个任务，1.回复用户说的内容 2. 用中文总结用户的语法错误 3. 在自己的回答中向用户推荐3-10个相关新词汇。\n 你在给出评价时的回复要求如下：1.不要使用Markdown格式 2.用Reply,Error,Vocabulary三个板块，分别表示你的回复，总结的错误和新的词汇，生成一段json你的回答。 3.在Reply模块，单独放置一个字符串，表示你的回复。 4.在Error板块，使用中文总结所有的问题，包括语法词汇问题。 5.在 Vocabulary板块，分别输出所有你推荐的单词。"}]
     while input_content!='exit':
         messages.append({"role": "user", "content": input_content})
         response = get_response(messages)
